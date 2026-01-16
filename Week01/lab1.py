d72b5f50daf02f5d53b61dc4ffd30b556b482be5
D81979 <101581979@georgebrown.ca>
2026-01-16 13:31:47 -0500

    add lab 01

---
+++ Week01/lab1.py
@@ -0,0 +1,25 @@
+# Smaple Coding Question 01 and 01
+# Disha Padsala
+
+
+# Question 2: defining an array (list)
+my_array = [1, 4, 7, 9]
+
+# Question 3: Order of Operations
+a = 1
+b = 2
+c = 3
+d = 4
+
+# Fully-Bracketed version of: e = a - b ** c // d + a % c
+e = (a - ((b ** c) // d)) + (a % c)
+print("Queation 3 result:", e)
+
+# Question 4: Formatting
+temperature = 32.6
+print("The temperature today is: {:.3f} degree Celsius".format(temperature))
+
+# Question 5: User Input
+userAge = int(input("Enter your age: "))
+userAge = userAge + 22
+print("Now showing the shop items filtred by age:", userAge)
