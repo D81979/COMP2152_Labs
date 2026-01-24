0088848d63d72fe9bdbce8153573cb29cd8185de
D81979 <101581979@georgebrown.ca>
2026-01-24 13:35:11 -0500

    add W02 Solution

--- Week02/Lab02.py
+++ Week02/Lab02.py
@@ -3,7 +3,7 @@ import random
 
 choice = ["Rock", "Paper", "Scissor"]
 
-playerChoice = input("Enter a number between 1 to 3 for the following choices: 1-Rock, 2-Paper, 3-Scissors: ")
+playerChoice = input("Enter a number between 1 to 3 for the following choices: 1-Rock, 2-Paper, 3-Scissors: ") # 1, 2, 3
 
 playerChoice = int(playerChoice)
 
@@ -13,12 +13,16 @@ else:
     # Develop the gae logic using if/elif/else
     computerChoice = random.randint(1, 3)
 
+#    playerChoiceIndex = choices[playerChoice - 1] # ==> 0, 1, 2
+#    computerChoiceIndex = choices[computerChoice - 1]
+#    print(playerChoiceIndex,  computerChoiceIndex)
+
     if playerChoice == computerChoice:
         print("It's tie!")
     elif playerChoice == 1 and computerChoice == 3:
         print("Rock beats Scissor - You win!")
     elif playerChoice == 2 and computerChoice == 1:
-        print("Paper beat Rock - You win!")
+        print("Paper beats Rock - You win!")
     elif playerChoice == 3 and computerChoice == 2:
         print("Scissor beats Paper - You win!")
     else:
