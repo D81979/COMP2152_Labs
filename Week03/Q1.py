d4e64853ab502989fac70a867c249a61c54c56dd
D81979 <101581979@georgebrown.ca>
2026-01-30 11:57:33 -0500

    Add W3

---
+++ Week03/Q1.py
@@ -0,0 +1,9 @@
+# Q1
+grades = [85, 92, 78, 95, 88]
+grades.append(90)
+print(grades)
+grades.sort()
+print(f"Sorted grade: {grades}")    # print(f"Sorted grade:", grades) 
+print(f"Highest grades: {grades[-1]}")
+print(f"Lowest grades: {grades[0]}")
+print(f"Total number of grades: {ten(grades)}")
---
+++ Week03/Q5.py
@@ -0,0 +1,15 @@
+contacts = {
+    "Alice": "555-12234",
+    "Bob": "555-5678",
+    "Charlie": "555-9999"
+}
+print(f"Alice's number: {contacts["Alice"]}")
+contacts["Diana"] = "555-4321"
+print(f"Contacts after adding Diana: {contacts}")
+contacts["Bob"] = "555-0000"
+print(f"Contact after updating Bob: {contacts}")
+del contacts["Charlie"]
+print(f"Contacts afer deleting Charlie: {contacts}")
+print(f"All names {contacts.keys()}")
+print(f"All number {contacts.values()}")
+print(f"Total contacts {len(contacts)}")
